1d43d7f9c24fa15f4324c9bfb2a83532ccd3f711
Eric Roberson <fcs2014eroberson@gmail.com>
2024-02-08 15:39:36 -0700

    addd oauth to snippets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
 	id("org.springframework.boot") version "3.2.1"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "com.snippets"
@@ -22,14 +22,19 @@ repositories {
 }
 
 dependencies {
+	implementation("com.okta.spring:okta-spring-boot-starter:3.0.5")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.modelmapper:modelmapper:2.1.1")
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
 	implementation("javax.xml.bind:jaxb-api:2.3.0")
 	implementation("com.fasterxml.jackson.core:jackson-databind")
+	implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation ("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	implementation ("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.postgresql:postgresql")
--- src/main/java/com/snippets/controllers/SnippetController.java
+++ src/main/java/com/snippets/controllers/SnippetController.java
@@ -57,14 +57,14 @@ public class SnippetController {
         //object mapper to convert JSON data to Java object
         ObjectMapper mapper = new ObjectMapper();
         Snippet[] snippets = mapper.readValue(snippetResource, Snippet[].class);
-//        TextEncryptor encryptor = Encryptors.text(encryptionPass, encryptionSalt);
-//
-//        //these lines were throwing errors earlier because it was trying to de-crypt non-encrypted data from the old seed data
-//
-//        for (int i = 0; i < snippets.length; i++) {
-//            String decryptedCode = encryptor.decrypt(snippets[i].getCode());
-//            snippets[i].setCode(decryptedCode);
-//        }
+        TextEncryptor encryptor = Encryptors.text(encryptionPass, encryptionSalt);
+
+        //these lines were throwing errors earlier because it was trying to de-crypt non-encrypted data from the old seed data
+
+        for (int i = 0; i < snippets.length; i++) {
+            String decryptedCode = encryptor.decrypt(snippets[i].getCode());
+            snippets[i].setCode(decryptedCode);
+        }
 
         return new ResponseEntity<>(snippets, HttpStatus.OK);
         //JpaRespository implementation: for when linked to "real" db
